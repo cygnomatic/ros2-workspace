ed5e2946dfdff4a9ab9a7f80010b2553e87f2f72
catslashbin <catslashbin@gmail.com>
2024-01-18 05:48:33 +0000

    Auto detect host env and kick off the correct docker variants

---
+++ .devcontainer/setup_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if command -v nvidia-smi &> /dev/null; then
+    HAS_GPU="enable"
+else
+    HAS_GPU="disable"
+fi
+
+if grep -q microsoft /proc/version; then
+    OS="win"
+else
+    OS="unix"
+fi
+
+ln -sf "$(pwd)/.devcontainer/docker-compose.gpu-${HAS_GPU}.yaml" "$(pwd)/.devcontainer/docker-compose.gpu.yaml"
+ln -sf "$(pwd)/.devcontainer/docker-compose.os-${OS}.yaml" "$(pwd)/.devcontainer/docker-compose.os.yaml"
