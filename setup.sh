0f3e5a346cba06a5bd4e4bd1bd69b497ba9131d9
catslashbin <catslashbin@gmail.com>
2024-01-21 10:00:50 +0000

    Add upstream remote and check for updates from upstream during setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,24 @@ set -e
 
 git submodule init
 git submodule update
+
 sudo apt-get update
+
 rosdep update --rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -y --rosdistro=$ROS_DISTRO
+
+# Add upstream remote if not exist
+git remote get-url upstream &>/dev/null
+if [ $? -ne 0 ]; then
+    git remote add upstream git@codeup.aliyun.com:cygnomatic/dev_ws/vscode_ros2_workspace.git
+fi
+git fetch upstream
+
+# Check if there is any update from upstream
+BEHIND_COUNT=$(git rev-list --count main..upstream/main)
+if [ $BEHIND_COUNT -gt 0 ]; then
+    echo -e "\e[1mYour main branch is behind upstream/main by $BEHIND_COUNT commits.\e[0m"
+    echo -e "\e[1mPlease merge the changes by running task \e[38;5;208m\u001b[4msync upstream\e[0m\e[1m.\e[0m"
+else
+    echo "The main branch is up to date with upstream/main."
+fi
