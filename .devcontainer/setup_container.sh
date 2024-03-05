e17bc13623d70d961e4a1256c740f6b9cc1184cf
catslashbin <catslashbin@gmail.com>
2024-03-05 13:04:26 +0000

    Add do not modify notes in generated docker-compose file

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -18,6 +18,8 @@ fi
 echo -e "Active container variant: GPU = \e[1m${HAS_GPU}\e[0m, OS = \e[1m${OS}\e[0m"
 cp -f "$(pwd)/.devcontainer/docker-compose.gpu-${HAS_GPU}.yaml" "$(pwd)/.devcontainer/docker-compose.gpu.yaml"
 cp -f "$(pwd)/.devcontainer/docker-compose.os-${OS}.yaml" "$(pwd)/.devcontainer/docker-compose.os.yaml"
+sed -i '1i##########################################################################################\n# DO NOT MODIFY THE CONTENTS OF THIS FILE! The contents of this file are automatically\n# generated and replaced during the devcontainer initialization process. To make changes, \n# please edit `docker-compose.gpu-xxx.yaml` instead.\n##########################################################################################' .devcontainer/docker-compose.gpu.yaml
+sed -i '1i##########################################################################################\n# DO NOT MODIFY THE CONTENTS OF THIS FILE! The contents of this file are automatically\n# generated and replaced during the devcontainer initialization process. To make changes, \n# please edit `docker-compose.os-xxx.yaml` instead.\n##########################################################################################' .devcontainer/docker-compose.os.yaml
 
 # Ignore changes to auto-generated docker-compose files
 git update-index --assume-unchanged .devcontainer/docker-compose.gpu.yaml
