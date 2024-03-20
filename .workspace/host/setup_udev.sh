092f7559ae123d86ac7d2b957f5338e7db203cde
catslashbin <catslashbin@gmail.com>
2024-03-20 22:04:03 +0800

    Add udev for serial tegra

--- .workspace/host/setup_udev.sh
+++ .workspace/host/setup_udev.sh
@@ -1,1 +1,2 @@
 echo 'SUBSYSTEMS=="usb", ATTR{idVendor}=="2bdf", MODE="0777"' | sudo tee -a /usr/lib/udev/rules.d/65-hik-camera.rules > /dev/null
+echo 'KERNEL=="ttyTHS0", SUBSYSTEM=="tty", DRIVERS=="serial-tegra", MODE="0777"' | sudo tee -a /usr/lib/udev/rules.d/65-tegra-serial.rules > /dev/null
